00504396f2d5a83a8ea6ecef327ba847bca3fdb7
Catayao56 <Catayao56@gmail.com>
2017-12-20 09:44:36 +0800

    Modified README files, deleted placeholders, and added scripts.
    Committed changes:
    
    MOD README
    MOD README-DEV
    DEL src/SOFTWARE-EXECUTABLES
    ADD src/install.sh
    ADD src/termux-pkg-backup
    ADD src/termux-pkg-restore
    ADD src/termux-pkg-uninstall

---
+++ src/install.sh
@@ -0,0 +1,28 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+echo ""
+echo "TERMUX BACKUP"
+echo ""
+echo "Installing..."
+apt update -y
+apt install -y bash apt dpkg
+cp 'termux-pkg-backup' '$PREFIX/bin/'
+cp 'termux-pkg-restore' '$PREFIX/bin/'
+cp 'termux-pkg-uninstall' '$PREFIX/bin/'
+chmod +x '$PREFIX/bin/termux-pkg-backup'
+chmod +x '$PREFIX/bin/termux-pkg-restore'
+chmod +x '$PREFIX/bin/termux-pkg-uninstall'
+echo "Installing... Done!"
+echo ""
+echo "USAGE:"
+echo ""
+echo "Backup:"
+echo "termux-pkg-backup [FILENAME OF BACKUP FILE TO BE CREATED]"
+echo ""
+echo "Restore:"
+echo "termux-pkg-restore [FILENAME OF BACKUP FILE TO BE RESTORED]"
+echo ""
+echo "Uninstall Termux Backup:"
+echo "termux-pkg-uninstall"
+echo ""
+exit 0
